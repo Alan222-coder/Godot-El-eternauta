5b6883af1508fd2c006b31652ec154802227cd03
Angrymanaboutcomp <162182803+Angrymanaboutcomp@users.noreply.github.com>
2026-05-03 23:01:46 -0300

    Enemigo basico
    Añadido enemigo que va a la direccion del jugador

---
+++ enemy.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed := 100
+var player: Node2D = null
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+
+func _physics_process(delta):
+	if player == null:
+		return
+	
+	var direction = (player.global_position - global_position).normalized()
+	velocity.x = direction.x * speed
+	
+	move_and_slide()
